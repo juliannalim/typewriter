434b772c6182c1fe6e93ae1d72ed3ecbb244e8ad
Julianna <julianna.j.lim@gmail.com>
2020-06-30 13:27:47 -0400

    animated sentence

---
+++ typewriter.js
@@ -0,0 +1,11 @@
+const sentence = "hello there from lighthouse labs";
+
+let delay = 0;
+  // print the char here
+for (const char of sentence) {
+  setTimeout(() => process.stdout.write(char), delay);
+  delay += 50;
+
+} // <= 1s delay to make it noticeable. Can dial it down later.
+
+setTimeout(() => console.log(), delay);
